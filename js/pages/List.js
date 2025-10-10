dceae00e85aff9bd74f40d7c8db5a1a818d03612
Lainfelled <noticehowwhen@gmail.com>
2025-10-10 13:38:32 +0800

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -101,7 +101,7 @@ export default {
                     <h3>Submission Requirements</h3>
                    <a href="https://docs.google.com/document/d/1rLfbq4w_QBSGRm7-j5TchKiR0f4bzhB8VVEEGxHhXZs/edit?usp=drivesdk" 
    target="_blank" 
-   style="color: blue; text-decoration: underline; font-weight: bold;">
+   style="color: blue; text-decoration: underline;">
   Rules Documents Here
 </a>
                 </div>
