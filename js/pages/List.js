c792cd0cd034bd9781971607b238cd24a6cc0a25
snowfall <frickavernusratetidalwave@gmail.com>
2025-10-12 12:05:32 -0600

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -101,7 +101,7 @@ export default {
                     <h3>Submission Requirements</h3>
                    <a href="https://docs.google.com/document/d/1rLfbq4w_QBSGRm7-j5TchKiR0f4bzhB8VVEEGxHhXZs/edit?usp=drivesdk" 
    target="_blank" 
-   style="color: black; font-family: Arial;">
+   style="color: black; font-family: Inter;">
   Rules Documents Here
 </a>
                 </div>
